55225eb5085b929791e3e990949d7ccb28de2c2c
Minki Kang <zzxc1133@gmail.com>
2025-05-26 14:10:06 +0900

    Delete google_test.py

--- google_test.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="")
-
-question = "The proper divisors of 12 are 1, 2, 3, 4 and 6. A proper divisor of an integer $N$ is a positive divisor of $N$ that is less than $N$. What is the sum of the proper divisors of the sum of the proper divisors of 284?"
-response = client.models.generate_content(
-    model="gemini-2.5-flash-preview-05-20", contents=question
-)
-print(response.text)
